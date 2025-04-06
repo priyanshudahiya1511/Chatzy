1f5d19ecfbef1f5641031885f76df2491c60d114
priyanshudahiya1511 <priyanshudahiya1234@gmail.com>
2025-04-06 17:24:33 +0530

    Deployment

--- Backend/server.js
+++ Backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import express from "express";
 import dotenv from "dotenv";
 import { app, server } from "./socket/socket.js";
@@ -11,9 +12,9 @@ import userRoutes from "./routes/user.routes.js";
 import connecttoMongoDB from "./db/connecttoMongoDB.js";
 import cookieParser from "cookie-parser";
 
-
-
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve()
 
 dotenv.config();
 
@@ -22,13 +23,13 @@ app.use(cookieParser());
 
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
-app.use("/api/users",userRoutes)
+app.use("/api/users",userRoutes);
 
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
 
-app.get('/',(req,res)=>{
-    res.send("Hello World")
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"frontend", "dist", "index.html"))
 })
-
 
 server.listen(PORT,()=>{
     connecttoMongoDB();
--- frontend/components/sidebar/Conversations.jsx
+++ frontend/components/sidebar/Conversations.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Conversation from './Conversation';
 import useGetConversations from '../../hooks/useGetConversations';
 import {getRandomEmoji} from '../../src/utils/emojis';
 
 const Conversations = () => {
 
-  const {loading,conversations} = useGetConversations()
- 
+  const {loading,conversations} = useGetConversations();
+  
   return (
     <div className='py-2 flex flex-col overflow-auto scrollbar'>
         {loading ? (<span className='loading loading-spinner mx-auto'></span>)   :
