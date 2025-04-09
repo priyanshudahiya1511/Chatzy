3fa6d5e9d1bb55337673997f7873a40beb8a8c82
priyanshudahiya1511 <priyanshudahiya1234@gmail.com>
2025-04-09 16:50:18 +0530

    Updated

--- frontend/components/messages/Message.jsx
+++ frontend/components/messages/Message.jsx
@@ -12,7 +12,7 @@ const Message = ({message}) => {
   const formattedTime = extractTime(message.createdAt)
   const chatClassName = fromMe ? "chat-end" : "chat-start";
   const profilePic = fromMe ? authUser.profilepic : selectedConversation?.profilepic;
-  const bubbleBgColor = fromMe ? "bg-blue-500" : "";
+  const bubbleBgColor = fromMe ? "bg-blue-500" : "bg-gray-200 text-black dark:bg-gray-800 dark:text-white";
 
   const shakeClass = message.shouldShake ? "shake" :""
 
