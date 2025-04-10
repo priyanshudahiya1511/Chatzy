d30c3b3b0f0f012677a54ef9a80802ed15d6516d
priyanshudahiya1511 <priyanshudahiya1234@gmail.com>
2025-04-10 19:11:39 +0530

    done

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+    darkMode: 'class', // ← this enables dark mode using a "dark" class on <html> or <body>
     content: [
       "./index.html",
       "./src/**/*.{js,ts,jsx,tsx}",
